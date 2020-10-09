3722b74b4e0ec06845c4d2f90aa054d46bb01c4b
EdsonOli <edso06@gmail.com>
2020-10-09 14:57:43 -0300

    ..

--- src/projeto_sorvil/controller/ControladorCards.java
+++ src/projeto_sorvil/controller/ControladorCards.java
@@ -2,14 +2,29 @@
 package projeto_sorvil.controller;
 
 import projeto_sorvil.dados.CardRepositorio;
+import projeto_sorvil.dados.RepositorioCards;
 
 public class ControladorCards {
     
-    private CardRepositorio repositorioCards;
-
-    public ControladorCards() {
+    private RepositorioCards repositorioCards;
+    private static ControladorCards instance;
+    
+    public static ControladorCards getInstance() {
+        if (instance == null) {
+            instance = new ControladorCards();
+        }
+        return instance;
+    }
+    
+    private ControladorCards() {
         this.repositorioCards = CardRepositorio.getInstance();
     }
+    
+
+    
+
+
+
 
     
 }
