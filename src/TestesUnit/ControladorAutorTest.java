62ddc450cfcccf4d52036d7ac55ebe6beac33173
EdsonOli <edso06@gmail.com>
2020-10-12 15:57:23 -0300

    bora fazer teste

---
+++ src/TestesUnit/ControladorAutorTest.java
@@ -0,0 +1,29 @@
+package TestesUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControladorAutorTest {
+
+	@Test
+	void testControladorAutor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNovoAutor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBuscarPorNome() {
+		fail("Not yet implemented");
+	}
+
+}
