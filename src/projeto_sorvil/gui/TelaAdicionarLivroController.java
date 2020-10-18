09957faa2381b2fd9b81b4d2f2f7990c682fd603
Rayanealves <rayanea@outlook.com>
2020-10-18 15:12:29 -0300

    checkbox

--- src/projeto_sorvil/gui/TelaAdicionarLivroController.java
+++ src/projeto_sorvil/gui/TelaAdicionarLivroController.java
@@ -1,18 +1,13 @@
 package projeto_sorvil.gui;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
-import projeto_sorvil.model.Genero;
+
 
 public class TelaAdicionarLivroController {
     
@@ -136,16 +131,13 @@ public class TelaAdicionarLivroController {
     @FXML
     private CheckBox turismo;
     
+    @FXML
+    private Button btnAbrir;
+    
     
     @FXML
     public void initialize() {
       
     }
-    
-    
-    
-    
-    
-
-   
+  
 }
