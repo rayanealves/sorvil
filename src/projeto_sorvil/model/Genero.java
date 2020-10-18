1e19d980ed98d3e926a2e0a54f1c1db00b115efb
Rayanealves <rayanea@outlook.com>
2020-10-17 22:20:45 -0300

    classe genero

--- src/projeto_sorvil/model/Genero.java
+++ src/projeto_sorvil/model/Genero.java
@@ -2,9 +2,28 @@ package projeto_sorvil.model;
 
 
 public enum Genero {
-	ADMINISTRACAO, ARTES, AUTOAJUDA, AVENTURA, BIOGRAFIA, CONTO, DICIONARIO, 
-	DIREITO, ECONIMIA, FANTASIA, FICCAO_CIENTIFICA, FILOSOFIA, GEOGRAFIA, HISTORIA,
-	HUMOR, LINGUISTICA, MEDICINA, POESIA, POLICIAL, PSICOLOGIA, RELIGIAO,
-	ROMANCE, SUSPENSE, TERROR, TURISMO
+	ADMINISTRACAO("Administração"), ARTES("Artes"), AUTOAJUDA("Autoajuda"), 
+        AVENTURA("Aventura"), BIOGRAFIA("Biografia"), CONTO("Conto"), DICIONARIO("Dicionario"), 
+	DIREITO("Direito"), ECONIMIA("Economia"), FANTASIA("Fantasia"), FICCAO_CIENTIFICA("Ficção Científica"), 
+        FILOSOFIA("Filosofia"), GEOGRAFIA("Geografia"), HISTORIA("História"),
+	HUMOR("Humor"), LINGUISTICA("Linguistica"), MEDICINA("Medicina"), POESIA("Poesia"), POLICIAL("Policial"), 
+        PSICOLOGIA("Psicologia"), RELIGIAO("Religião"), ROMANCE("Romance"), SUSPENSE("Suspense"), 
+        TERROR("Terror"), TURISMO("Turismo");
+  
+        private String nome;
+        
+        Genero(String nome){
+            this.nome = nome;
+        }
+        
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+        
+        
 
 }
