c5f90ada10f12c0421c4c56bef9a28843842a275
Rayanealves <rayanea@outlook.com>
2020-10-19 15:09:22 -0300

    mais mudanca

--- src/projeto_sorvil/gui/TelaBuscarLivroController.java
+++ src/projeto_sorvil/gui/TelaBuscarLivroController.java
@@ -7,7 +7,12 @@ package projeto_sorvil.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,9 +21,18 @@ import javafx.fxml.Initializable;
  */
 public class TelaBuscarLivroController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+     @FXML
+    private Text nometxt;
+
+    @FXML
+    private TextField livro;
+
+    @FXML
+    private Label aviso;
+
+    @FXML
+    private Button btnBuscar;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
