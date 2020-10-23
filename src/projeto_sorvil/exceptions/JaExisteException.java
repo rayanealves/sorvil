a45e0daf270c8cfb6ce2d740e7909cf6a8da05a9
EdsonOli <edso06@gmail.com>
2020-10-23 18:50:32 -0300

    começando exceptions

---
+++ src/projeto_sorvil/exceptions/JaExisteException.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+public class JaExisteException extends Exception {
+	
+
+private static final long serialVersionUID = -3040088524270012847L;
+private Object objeto;
+    
+    public JaExisteException(Object obj) {
+        super("Elemento já existe");
+        this.objeto = obj;        
+    }
+
+	public Object getObjeto() {
+		return objeto;
+	}
+
+	public void setObjeto(Object objeto) {
+		this.objeto = objeto;
+	}
+    
+	@Override
+	public String toString() {
+		return "JaExisteException [objeto=" + objeto + "]";
+	}
+
+}
---
+++ src/projeto_sorvil/exceptions/NaoExisteException.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+public class NaoExisteException extends Exception{
+	private static final long serialVersionUID = -7579257249288556574L;
+	private Object objeto;
+	
+	public NaoExisteException(Object obj) {
+		super("Elemento não existente");
+		this.objeto = obj;
+	}
+
+	public Object getObjeto() {
+		return objeto;
+	}
+
+	public void setObjeto(Object objeto) {
+		this.objeto = objeto;
+	}
+}
