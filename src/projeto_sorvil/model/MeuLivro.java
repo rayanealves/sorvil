8109b76d875e6b1cab7b8f8e7091dd5145f881f5
EdsonOli <edso06@gmail.com>
2020-10-31 17:07:57 -0300

    é isso2

--- src/projeto_sorvil/model/MeuLivro.java
+++ src/projeto_sorvil/model/MeuLivro.java
@@ -8,6 +8,10 @@ public class MeuLivro implements Serializable{
     private static final long serialVersionUID = 1955524342716143140L;
     private int nota;
     private Status status;
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
     private boolean favorito = false;
     private Livro livro;
     
