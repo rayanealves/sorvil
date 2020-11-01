ffdf021b27834278da297f01c9836dd1143c93ab
EdsonOli <edso06@gmail.com>
2020-11-01 18:37:34 -0300

    adições

--- src/projeto_sorvil/model/MeuLivro.java
+++ src/projeto_sorvil/model/MeuLivro.java
@@ -55,7 +55,7 @@ public class MeuLivro implements Serializable{
 	@Override
 	public String toString() {
 		return "====================\n" + livro.toString() + "\n Nota: " + nota
-                        + " | Status: " + status + "\n, favorito: "
+                        + " | Status: " + status + "\nFavorito: "
                         + favorito + "\n====================";
 	}
 
